62881d116b6aa9e41c076f8c7e984fd4e9113b75
itenpascal <90401249+itenpascal@users.noreply.github.com>
2022-10-18 14:29:43 +0200

    initiale Files ohne Inhalt

---
+++ Receiver/Core/Inc/display.h
@@ -0,0 +1,84 @@
+/**
+ * @file   display.h
+ *
+ * @brief  	Brief description of the content of display.h
+ * @author 	itenp, Juventus Techniker Schule
+ * @date   	Oct 13, 2022 - first implementation
+ * @version 1.0.0
+ * 
+ * MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Copyright (c) 2022 Juventus Techniker Schule
+ */
+ 
+#ifndef INC_TEMPLATE_H_
+#define INC_TEMPLATE_H_
+
+ // ********************************************************************************
+ /**
+ * @brief  Includes
+ *
+ * Section for module-specific include files
+ * - Normally not necessary in header files
+ */
+
+ 
+ // ********************************************************************************
+ /**
+ * @brief  Macros
+ *
+ * Section for module-specific definitions (constants) and macros
+ */
+
+ 
+ // ********************************************************************************
+ /**
+ * @brief  Enumerations
+ *
+ * Section for module-specific enumerations
+ */
+
+ 
+ // ********************************************************************************
+ /**
+ * @brief  Structures
+ *
+ * Section for module-specific structures
+ */
+
+ 
+ // ********************************************************************************
+ /**
+ * @brief  Global variables
+ *
+ * Section for module-specific global variables
+ */
+
+ 
+ // ********************************************************************************
+ /**
+ * @brief  Global function prototypes
+ *
+ * Section for module-specific global function prototypes
+ */
+ 
+
+ #endif /* END: display.h */
